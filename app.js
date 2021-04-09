f00ad8c1a6d76c359a169b8630d99cdf7b3805f2
KeerthiMuli <77706824+KeerthiMuli@users.noreply.github.com>
2021-04-09 13:22:28 -0500

    Attributes has been updated

--- app.js
+++ app.js
@@ -60,7 +60,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/resource/park', parkRouter);
+app.use('/park', parkRouter);
 app.use('/stars', starRouter);
 app.use('/slot', slotRouter);
 app.use('/resource', resourceRouter);
--- controllers/park.js
+++ controllers/park.js
@@ -10,10 +10,19 @@ exports.park_list = async function(req, res) {
     }
 
 };
-// for a specific park.
-exports.park_detail = function(req, res) {
-res.send('NOT IMPLEMENTED: park detail: ' + req.params.id);
+// for a specific Park.
+exports.park_detail = async function(req, res) {
+    console.log("detail"  + req.params.id)
+    try {
+        result = await park.findById( req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
+
+
 // Handle park create on POST.
 exports.park_create_post = async function(req, res) {
     console.log(req.body)
@@ -36,10 +45,24 @@ exports.park_create_post = async function(req, res) {
 exports.park_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: park delete DELETE ' + req.params.id);
 };
-// Handle park update form on PUT.
-exports.park_update_put = function(req, res) {
-res.send('NOT IMPLEMENTED: park update PUT' + req.params.id);
+// Handle Park update form on PUT.
+exports.park_update_put = async function(req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await park.findById( req.params.id)
+        // Do updates of properties
+        if(req.body.Name) toUpdate.Name = req.body.Name;
+        if(req.body.EntryFee) toUpdate.EntryFee = req.body.EntryFee;
+        if(req.body.City) toUpdate.City = req.body.City;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
+    }
 };
+
 // VIEWS
 // Handle a show all view
 exports.park_view_all_Page = async function(req, res) {
